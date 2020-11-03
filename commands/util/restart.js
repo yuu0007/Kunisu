63a62b1286bc62bb186670a504a11ef618eca98f
yuu0007 <jmquimosing@icloud.com>
2020-11-03 09:22:18 +0000

    hopefully

---
+++ commands/util/restart.js
@@ -0,0 +1,19 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'restart',
+			memberName: 'restart',
+			group: 'util',
+			ownerOnly: true,
+			guarded: true,
+			description: 'Attempts to restart the bot if its running under pm2.'
+		});
+	}
+	
+        async run(msg) {
+		msg.react('✔')
+		process.exit();
+	}
+};
