92f6625738ba3abf821196b7ac3711fc089cf31e
yuu0007 <jmquimosing@icloud.com>
2020-11-10 08:55:00 +0000

    h

--- commands/util/exec.js
+++ commands/util/exec.js
@@ -1,31 +1,32 @@
-const { Command } = require('discord.js-commando');
-const { execSync } = require('child_process');
-const { stripIndents } = require('common-tags');
+const { Command } = require("discord.js-commando");
+const { execSync } = require("child_process");
+const { stripIndents } = require("common-tags");
 
 module.exports = class ExecCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'exec',
-			aliases: ['execute', '$'],
-			group: 'util',
-			memberName: 'exec',
-			description: 'Executes a command-line application.',
+			name: "exec",
+			aliases: ["execute", "$"],
+			group: "util",
+			memberName: "exec",
+			description: "Executes a command-line application.",
 			ownerOnly: true,
 			guarded: true,
 			args: [
 				{
-					key: 'command',
-					prompt: 'What command do you want to execute?',
-					type: 'string'
-				}
-			]
+					key: "command",
+					prompt:
+						"What command do you want to execute?",
+					type: "string",
+				},
+			],
 		});
 	}
 
 	async run(msg, { command }) {
 		const results = await this.exec(command);
 		return msg.reply(stripIndents`
-			_${results.err ? 'An error occurred:' : 'Successfully executed.'}_
+			_${results.err ? "An error occurred:" : "Successfully executed."}_
 			\`\`\`sh
 			${results.std}
 			\`\`\`
@@ -34,7 +35,10 @@ module.exports = class ExecCommand extends Command {
 
 	async exec(command) {
 		try {
-			const stdout = await execSync(command, { timeout: 30000, encoding: 'utf8' });
+			const stdout = await execSync(command, {
+				timeout: 30000,
+				encoding: "utf8",
+			});
 			return { err: false, std: stdout.trim() };
 		} catch (err) {
 			return { err: true, std: err.stderr.trim() };
