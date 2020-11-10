2cee58240a607b00303c91931530ecc6d3c432a9
yuu0007 <jmquimosing@icloud.com>
2020-11-10 03:52:15 +0000

    added exec cmd

---
+++ commands/util/exec.js
@@ -0,0 +1,43 @@
+const { Command } = require('discord.js-commando');
+const { execSync } = require('child_process');
+const { stripIndents } = require('common-tags');
+
+module.exports = class ExecCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'exec',
+			aliases: ['execute', '$'],
+			group: 'util',
+			memberName: 'exec',
+			description: 'Executes a command-line application.',
+			ownerOnly: true,
+			guarded: true,
+			args: [
+				{
+					key: 'command',
+					prompt: 'What command do you want to execute?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { command }) {
+		const results = await this.exec(command);
+		return msg.reply(stripIndents`
+			_${results.err ? 'An error occurred:' : 'Successfully executed.'}_
+			\`\`\`sh
+			${results.std}
+			\`\`\`
+		`);
+	}
+
+	async exec(command) {
+		try {
+			const stdout = await execSync(command, { timeout: 30000, encoding: 'utf8' });
+			return { err: false, std: stdout.trim() };
+		} catch (err) {
+			return { err: true, std: err.stderr.trim() };
+		}
+	}
+};
